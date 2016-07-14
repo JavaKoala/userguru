86293e0e3286fa66c257578b2986cabbb4bf3294
Ben Meyer <bmeyer@medsagetechnologies.com>
2016-07-13 22:56:44 -0400

    [users] - Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :user_roles
   has_many :roles, :through => :user_roles
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
