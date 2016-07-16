7acb3cdca3c58b2a0788a828865e65b03069e9f2
Ben Meyer <bmeyer@medsagetechnologies.com>
2016-07-16 16:56:50 -0400

    [users] - added sign-up form

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -14,8 +14,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
   test "layout links test" do
     get root_path
-    # get this to work when the internet is back
-    # assert_template 'static_pages/home'
+    assert_response :success
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", signup_path
