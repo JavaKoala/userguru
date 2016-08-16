7c869629474b879856bea76c761892c153317939
Ben Meyer <bmeyer@medsagetechnologies.com>
2016-08-16 07:29:10 -0400

    [issues] - Added error messages to new issues, issues controller tests.

--- test/controllers/issues_controller_test.rb
+++ test/controllers/issues_controller_test.rb
@@ -3,8 +3,8 @@ require 'test_helper'
 class IssuesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    # @user  = user(:one)
-    # @user.roles << Role.where(name: 'customer')
+    @user  = users(:one)
+    @user.roles << Role.where(name: 'customer')
     @issue = issues(:one)
   end
   
@@ -14,7 +14,45 @@ class IssuesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create new issue" do
-    # get new_issue_path
+    log_in_as(@user)
+    get new_issue_path
+    assert_difference 'Issue.count', 1 do
+      post issues_path, params: { issue: { title: "test title", 
+                                           description: "test description", 
+                                           user_id: @user.id } }
+      follow_redirect!
+    end
+    assert_response :success
+  end
+  
+  test "should not create issue when there is no title" do
+    log_in_as(@user)
+    get new_issue_path
+    assert_no_difference 'Issue.count' do
+      post issues_path, params: { issue: { title: "", 
+                                           description: "test description", 
+                                           user_id: @user.id } }
+    end
+  end
+  
+  test "should not create issue when there is no description" do
+    log_in_as(@user)
+    get new_issue_path
+    assert_no_difference 'Issue.count' do
+      post issues_path, params: { issue: { title: "test title", 
+                                           description: "", 
+                                           user_id: @user.id } }
+    end
+  end
+  
+  test "should not create issue when there is no user id" do
+    log_in_as(@user)
+    get new_issue_path
+    assert_no_difference 'Issue.count' do
+      post issues_path, params: { issue: { title: "test title", 
+                                           description: "test desciption", 
+                                           user_id: "" } }
+    end
   end
 
 end
