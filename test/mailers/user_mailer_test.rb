c9ffc8e6f0d614ae04d53292a650a248ea550795
Ben Meyer <bmeyer@medsagetechnologies.com>
2016-11-12 08:15:35 -0500

    [production_update] - Updated user_mailer_test for correct mail.from

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -7,7 +7,7 @@ class UserMailerTest < ActionMailer::TestCase
     mail = UserMailer.account_activation(user)
     assert_equal "Account activation",    mail.subject
     assert_equal [user.email],            mail.to
-    assert_equal ["from@example.com"],    mail.from
+    assert_equal ["no-reply@user.guru"],  mail.from
     assert_match user.name,               mail.body.encoded
     assert_match user.activation_token,   mail.body.encoded
     assert_match CGI::escape(user.email), mail.body.encoded
@@ -17,11 +17,11 @@ class UserMailerTest < ActionMailer::TestCase
     new_user = users(:one)
     admin_user = users(:two)
     mail = UserMailer.new_user_activated(new_user, admin_user)
-    assert_equal "New user activated", mail.subject
-    assert_equal [admin_user.email],   mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match new_user.name,        mail.body.encoded
-    assert_match new_user.email,       mail.body.encoded
+    assert_equal "New user activated",   mail.subject
+    assert_equal [admin_user.email],     mail.to
+    assert_equal ["no-reply@user.guru"], mail.from
+    assert_match new_user.name,          mail.body.encoded
+    assert_match new_user.email,         mail.body.encoded
   end
 
   test "password_reset" do
@@ -30,7 +30,7 @@ class UserMailerTest < ActionMailer::TestCase
     mail = UserMailer.password_reset(user)
     assert_equal "Password reset", mail.subject
     assert_equal [user.email], mail.to
-    assert_equal ["from@example.com"],    mail.from
+    assert_equal ["no-reply@user.guru"],  mail.from
     assert_match user.reset_token,        mail.body.encoded
     assert_match CGI::escape(user.email), mail.body.encoded
   end
