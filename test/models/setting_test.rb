89170c83ea0be856f428561e4018ba6f6d0cb35d
Ben Meyer <bmeyer@medsagetechnologies.com>
2016-11-29 21:04:23 -0500

    [user_configuration] - Updated settings fixtures to match setting seeds

--- test/models/setting_test.rb
+++ test/models/setting_test.rb
@@ -1,12 +1,9 @@
 require 'test_helper'
 
 class SettingTest < ActiveSupport::TestCase
-  
-  def setup
-    @setting = settings(:one)
-  end
 
   test "setting name should be present" do
+    @setting = Setting.first
     @setting.name = nil
     assert_not @setting.valid?
   end
