60e66778cee9dd6b38d0566a54ecd137fcef2070
Ben Meyer <bmeyer@medsagetechnologies.com>
2016-12-02 17:41:37 -0500

    [user_configuration] - Added settings update to settings table

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -7,7 +7,20 @@ class SettingsController < ApplicationController
       setting.value = setting_value(setting.name)
     end
   end
-  
+
   def update
+    @setting = Setting.find(params[:id])
+    if @setting.update_attributes(settings_params)
+      flash[:success] = "Setting updated"
+    else
+      flash[:danger] = @setting.errors.full_messages[0]
+    end
+    redirect_to settings_path
   end
+
+  private
+
+    def settings_params
+      params.require(:setting).permit(:value)
+    end
 end
--- test/controllers/settings_controller_test.rb
+++ test/controllers/settings_controller_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class SettingsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @setting = settings(:one)
+  end
+
+  test 'setting update should work' do
+    get settings_path
+    patch setting_path(@setting), params: { setting: { value: "updated setting" } } 
+    assert_redirected_to settings_path
+    assert_not flash.empty?
+    @setting.reload
+    assert @setting.value == "updated setting"
+  end
+
+  test 'settings update should not work if there is no value' do
+    get settings_path
+    assert_raises ("ParameterMissing") {
+      patch setting_path(@setting), params: { setting: nil }
+    }
+  end
+
+  test 'settings update should not work if there is no setting' do
+    get settings_path
+    @setting.id = 8675309
+    assert_raises ("RecordNotFound") {
+      patch setting_path(@setting), params: { setting: { value: "updated setting" } }
+    }
+  end
 end
