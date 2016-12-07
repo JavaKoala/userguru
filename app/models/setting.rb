165fe4ca946d0edf6bca3ecba70f1c90fecb4919
Ben Meyer <bmeyer@medsagetechnologies.com>
2016-12-07 07:03:11 -0500

    [valid_default_email] - Added email regex to default email setting

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,5 +2,12 @@ class Setting < ApplicationRecord
 
   # Model Validations
   validates :name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :value, format: { with: VALID_EMAIL_REGEX },
+                    if: :email_setting?,
+                    allow_blank: true
 
+  def email_setting?
+    name == "default_email"
+  end
 end
--- test/models/setting_test.rb
+++ test/models/setting_test.rb
@@ -7,4 +7,18 @@ class SettingTest < ActiveSupport::TestCase
     @setting.name = nil
     assert_not @setting.valid?
   end
+
+  test "default_email should be blank or an email address" do
+    # The default email setting is the fourth setting
+    @setting = Setting.fourth
+    assert @setting.name = "default_email"
+    @setting.value = "I can haz email"
+    assert_not @setting.valid?
+    @setting.value = ""
+    assert @setting.valid?
+    @setting.value = "test@test"
+    assert_not @setting.valid?
+    @setting.value = "@test.com"
+    assert_not @setting.valid?
+  end
 end
