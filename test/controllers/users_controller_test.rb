f2f70f70aa6312aaa7924ad01f8f07206c7a4be2
Ben Meyer <bmeyer@medsagetechnologies.com>
2016-12-11 09:42:32 -0500

    [test_refactor] - Updated users_controller_test to use updated fixtures

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,12 +3,10 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   
   def setup
-    @user = users(:one)
-    @user.roles << Role.where(name: 'customer')
-    @other_user = users(:two)
-    @other_user.roles << Role.where(name: 'customer')
-    @other_issue = issues(:two)
-    @other_issue.user_issue = UserIssue.new
+    @admin_user = users(:admin_user)
+    @customer1  = users(:customer1)
+    @customer2  = users(:customer2)
+    add_roles_to_users
   end
 
   test "should get new" do
@@ -17,45 +15,44 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should redirect get when not logged in" do
-    get user_path(@user)
+    get user_path(@customer1)
     assert_not flash.empty?
     assert_redirected_to login_url
   end
 
   test "should redirect edit when not logged in" do
-    get edit_user_path(@user)
+    get edit_user_path(@customer1)
     assert_not flash.empty?
     assert_redirected_to login_url
   end
   
   test "should redirect update when not logged in" do
-    patch user_path(@user), params: { user: { name: @user.name, email: @user.email } }
+    patch user_path(@customer1), params: { user: { name: @customer1.name, email: @customer1.email } }
     assert_not flash.empty?
     assert_redirected_to login_url
   end
 
   test "should redirect edit when logged in as wrong user" do
-    log_in_as(@other_user)
-    get edit_user_path(@user)
+    log_in_as(@customer2)
+    get edit_user_path(@customer1)
     assert_redirected_to login_url
   end
   
   test "should redirect update when logged in as wrong user" do
-    log_in_as(@other_user)
-    patch user_path(@user), params: { user: { name: @user.name, email: @user.email } }
+    log_in_as(@customer2)
+    patch user_path(@customer1), params: { user: { name: @customer1.name, email: @customer1.email } }
     assert_redirected_to login_url
   end
 
   test "should redirect show when logged in with customer role" do
-    log_in_as(@user)
-    get user_path(@other_user)
+    log_in_as(@customer1)
+    get user_path(@customer2)
     assert_redirected_to login_url
   end
 
   test "should not redirect show with an admin user" do
-    @user.roles << Role.where(name: 'admin')
-    log_in_as(@user)
-    get user_path(@other_user)
+    log_in_as(@admin_user)
+    get user_path(@customer2)
     assert_response :success
   end
 end
