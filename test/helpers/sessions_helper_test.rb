59580a0e8fbd8a521c41a7781ecc85cde46e0100
Ben Meyer <bmeyer@medsagetechnologies.com>
2016-12-12 17:32:25 -0500

    [test_refactor] - Updated SessionsHelperTest to use new fixtures

--- test/helpers/sessions_helper_test.rb
+++ test/helpers/sessions_helper_test.rb
@@ -5,41 +5,44 @@ class SessionsHelperTest < ActionView::TestCase
   include SessionsHelper
 
   def setup
-    @user = users(:one)
-    @user.roles << Role.where(name: 'customer')
-    remember(@user)
+    @admin_user          = users(:admin_user)
+    @customer1           = users(:customer1)
+    @representative_user = users(:representative)
+    add_roles_to_users
+    remember(@customer1)
   end
   
   test "current_user returns right user when session is nil" do
-    assert_equal @user, current_user
+    assert_equal @customer1, current_user
     assert is_logged_in?
   end
 
   test "current_user returns nil when remember digest is wrong" do
-    @user.update_attribute(:remember_digest, User.digest(User.new_token))
+    @customer1.update_attribute(:remember_digest, User.digest(User.new_token))
     assert_nil current_user
   end
   
   test "current user should not be internal if they only have the customer role" do
+    log_in_as(@customer1)
     assert_not internal_user?
   end
-  
+
   test "current user should not be admin if they do not have the admin role" do
-    @user.roles << Role.where(name: 'representative')
+    log_in_as(@representative_user)
     assert_not admin_user?
   end
 
   test "current user should be internal and admin if they have the admin role" do
-    @user.roles << Role.where(name: 'admin')
+    log_in_as(@admin_user)
     assert internal_user?
     assert admin_user?
   end
 
   test "current user should be internal if they have the representative role" do
-    @user.roles << Role.where(name: 'representative')
+    log_in_as(@representative_user)
     assert internal_user?
   end
-  
+
   test "the user has to be logged in to be internal or admin" do
     log_out
     assert_not internal_user?
