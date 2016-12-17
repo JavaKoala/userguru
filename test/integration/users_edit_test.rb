47a375b197c24c2e311b167fde8a02ffaf7ba45d
Ben Meyer <bmeyer@medsagetechnologies.com>
2016-12-17 09:44:13 -0500

    [test_refactor] - Updated user_edit_test to use new fixtures

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -5,79 +5,78 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   include SessionsHelper
 
   def setup
-    @user = users(:one)
-    @user.roles << Role.where(name: 'customer')
-    @other_user = users(:two)
-    @other_user.roles << Role.where(name: 'customer')
+    @customer1  = users(:customer1)
+    @customer2  = users(:customer2)
+    @admin_user = users(:admin_user)
+    add_roles_to_users
   end
   
   test "unsuccessful edit" do
-    log_in_as(@user)
-    get edit_user_path(@user)
+    log_in_as(@customer1)
+    get edit_user_path(@customer1)
     assert_response :success
-    patch user_path(@user), params: { user: { name: '',
-                                              email: 'foo@invalid',
-                                              password: 'foo',
-                                              password_confirmation: 'bar' } }
-    assert_response :success                                     
+    patch user_path(@customer1), params: { user: { name: '',
+                                                   email: 'foo@invalid',
+                                                   password: 'foo',
+                                                   password_confirmation: 'bar' } }
+    assert_response :success
   end
   
   test "successful edit" do
-    log_in_as(@user)
-    get edit_user_path(@user)
+    log_in_as(@customer1)
+    get edit_user_path(@customer1)
     assert_response :success
     name = "Foo Bar"
     email = "foo@bar.com"
-    patch user_path(@user), params: { user: { name: name,
-                                              email: email,
-                                              password: "",
-                                              password_confirmation: "" } }
+    patch user_path(@customer1), params: { user: { name: name,
+                                                   email: email,
+                                                   password: "",
+                                                   password_confirmation: "" } }
     assert_not flash.empty?
-    assert_redirected_to @user
-    @user.reload
-    assert_equal @user.name, name
-    assert_equal @user.email, email                                 
+    assert_redirected_to @customer1
+    @customer1.reload
+    assert_equal @customer1.name, name
+    assert_equal @customer1.email, email
   end
   
   test "successful admin edit" do
-    log_in_as(@other_user)
-    get edit_user_path(@user)
+    log_in_as(@customer2)
+    get edit_user_path(@customer1)
     assert_redirected_to login_url
     delete logout_path
     follow_redirect!
     assert_not is_logged_in?
-    @user.roles << Role.where(name: 'admin')
-    log_in_as(@user)
-    get edit_user_path(@other_user)
+    log_in_as(@admin_user)
+    get edit_user_path(@customer2)
     assert_response :success
     name = "Foo Bar"
     email = "foo@bar.com"
     admin_role = Role.select(:id).where(name: 'admin')
     respresntative_role = Role.select(:id).where(name: 'representative')
     customer_role = Role.select(:id).where(name: 'customer')
-    patch user_path(@other_user), params: { user: { name: name,
-                                                    email: email,
-                                                    password: "",
-                                                    password_confirmation: "",
-                                                    role_id: [admin_role, respresntative_role, customer_role] } }
-    assert_redirected_to @other_user
+    patch user_path(@customer2), params: { user: { name: name,
+                                                   email: email,
+                                                   password: "",
+                                                   password_confirmation: "",
+                                                   role_id: [admin_role, respresntative_role, customer_role] } }
+    assert_redirected_to @customer2
   end
   
   test "successful edit with friendly forwarding" do
-    get edit_user_path(@user)
-    log_in_as(@user)
-    get edit_user_path(@user)
+    get edit_user_path(@customer1)
+    log_in_as(@customer1)
+    get edit_user_path(@customer1)
     assert_response :success
     name  = "Foo Bar"
     email = "foo@bar.com"
-    patch user_path(@user), params: { user: { name: name,
-                                              email: email,
-                                              password: "foobar",
-                                              password_confirmation: "foobar" } }
+    patch user_path(@customer1), params: { user: { name: name,
+                                                   email: email,
+                                                   password: "foobar",
+                                                   password_confirmation: "foobar" } }
     assert_not flash.empty?
-    assert_redirected_to @user
-    @user.reload
-    assert_equal @user.name, name
-    assert_equal @user.email, email
+    assert_redirected_to @customer1
+    @customer1.reload
+    assert_equal @customer1.name, name
+    assert_equal @customer1.email, email
   end
 end
