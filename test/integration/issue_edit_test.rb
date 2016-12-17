ed225f24f9e5c53df9953c1f03a22c18277e374b
Ben Meyer <bmeyer@medsagetechnologies.com>
2016-12-17 08:09:19 -0500

    [test_refactor] - Updated issue_edit_test to use new fixtures

--- test/integration/issue_edit_test.rb
+++ test/integration/issue_edit_test.rb
@@ -3,47 +3,44 @@ require 'test_helper'
 class IssueEditTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = users(:one)
-    @user.roles << Role.where(name: 'representative')
-    @other_user = users(:two)
-    @other_user.roles << Role.where(name: 'customer')
-    @issue = issues(:one)
-    @issue.user_issue = UserIssue.new
+    @representative_user = users(:representative)
+    @customer1           = users(:customer1)
+    add_roles_to_users
   end
-  
+
   test "issue edit test" do
-    log_in_as(@user)
+    log_in_as(@representative_user)
     get new_issue_path
     post issues_path, params: { issue: { title: "test title", 
                                          description: "test description", 
-                                         user_id: @user.id } }
+                                         user_id: @representative_user.id } }
     follow_redirect!
     assert_response :success
-    assert_select "a[href=?]", edit_issue_path, count: 1
+    @issue = Issue.find_by( title: "test title" )
+    assert_select "a[href=?]", edit_issue_path(@issue), count: 1
     assert_select "td", /test title/
     assert_select "td", /Open/
-    @issue = Issue.find_by( title: "test title" )
     get edit_issue_path(@issue)
     patch issue_path(@issue), params: { issue: { title: "updated title",
                                                  description: "updated description",
                                                  status: :closed,
-                                                 user_id: @user.id },
-                                        user_issue: { assigned_user: @user.id} }
+                                                 user_id: @representative_user.id },
+                                        user_issue: { assigned_user: @representative_user.id} }
     follow_redirect!
     assert_response :success
     assert_select "a[href=?]", edit_issue_path, count: 1
     assert_select "h1", /updated title/
     assert_select "li", /updated description/
     assert_select "li", /Closed/
-    assert_select "li", /Assigned to: #{@user.name}/
+    assert_select "li", /Assigned to: #{@representative_user.name}/
   end
   
   test "customers cannot edit the status and assigned user of issues" do
-    log_in_as(@other_user)
+    log_in_as(@customer1)
     get new_issue_path
     post issues_path, params: { issue: { title: "test title", 
                                          description: "test description", 
-                                         user_id: @other_user.id } }
+                                         user_id: @customer1.id } }
     follow_redirect!
     assert_response :success
     @issue = Issue.find_by( title: "test title" )
@@ -52,11 +49,11 @@ class IssueEditTest < ActionDispatch::IntegrationTest
   end
   
   test "unsuccessful edit, title and description empty" do
-    log_in_as(@user)
+    log_in_as(@customer1)
     get new_issue_path
     post issues_path, params: { issue: { title: "test title", 
                                          description: "test description", 
-                                         user_id: @user.id } }
+                                         user_id: @customer1.id } }
     follow_redirect!
     assert_response :success
     @issue = Issue.find_by( title: "test title" )
