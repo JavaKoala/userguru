861dfe32c823bc4e6bac11b4130238bb2117ec56
Ben Meyer <bmeyer@medsagetechnologies.com>
2016-12-17 10:02:29 -0500

    [test_refactor] - Updated users_login_test to use new fixtures

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -5,25 +5,25 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   include SessionsHelper
 
   def setup
-    @user = users(:one)
-    @user.roles << Role.where(name: 'customer')
+    @customer1 = users(:customer1)
+    add_roles_to_users
   end
   
   test "login with invalid information followed by logout" do
     get login_path
     assert_response :success
-    post login_path, params: { session: { email: @user.email,
+    post login_path, params: { session: { email: @customer1.email,
                                           password: 'password' } }
     assert is_logged_in?
     assert_not internal_user?
-    assert_redirected_to @user
+    assert_redirected_to @customer1
     follow_redirect!
     assert_response :success
     assert @users.nil?
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", issues_path, count: 0
-    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", user_path(@customer1)
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
@@ -31,16 +31,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
-    assert_select "a[href=?]", user_path(@user), count: 0
+    assert_select "a[href=?]", user_path(@customer1), count: 0
   end
   
   test "login with remembering" do
-    log_in_as(@user, remember_me: '1')
+    log_in_as(@customer1, remember_me: '1')
     assert_not_nil cookies['remember_token']
   end
   
   test "login without remembering" do
-    log_in_as(@user, remember_me: '0')
+    log_in_as(@customer1, remember_me: '0')
     assert_nil cookies['remember_token']
   end
 end
