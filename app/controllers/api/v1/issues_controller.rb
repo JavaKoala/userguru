5c6a108d93d735d37f68abd16921c80135529177
Ben Meyer <bmeyer@medsagetechnologies.com>
2017-03-18 10:26:15 -0400

    [authentication] - Added before_action to validate a current_api_user to issues_controller

--- app/controllers/api/v1/issues_controller.rb
+++ app/controllers/api/v1/issues_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::IssuesController < ApplicationController
+  before_action :current_api_user, only: :index
   respond_to :json
 
   def index
--- test/controllers/api/v1/issues_controller_test.rb
+++ test/controllers/api/v1/issues_controller_test.rb
@@ -1,13 +1,43 @@
 require 'test_helper'
 
-class Api::V1::IssuesControllerTest < ActionController::TestCase
+class Api::V1::IssuesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @customer1 = users(:customer1)
+    add_roles_to_users
+  end
+
+  test 'should not get index without authorization header' do
+    get api_v1_issues_path, params: { title: '' }
+    assert_response :unauthorized
+  end
+
+  test 'should not get index with wrong authorization header' do
+    get api_v1_issues_path, params: { title: '' },
+                            headers: { 'Authorization' => 'SUCKIT' }
+    assert_response :unauthorized
+  end
+
   test 'should get index' do
-    get :index, params: { title: '' }
+    get api_v1_issues_path, params: { title: '' }, 
+                            headers: { 'Authorization' => @customer1.auth_token }
+    assert_response :success
+  end
+
+  test 'should get index with authorization token creation' do
+    post api_v1_sessions_path, params: { session: { email: @customer1.email, password: 'password' } }
+    assert_response :success
+    body = JSON.parse(response.body)
+    @customer1.reload
+    customer1_auth_token = body['auth_token']
+    get api_v1_issues_path, params: { title: '' }, 
+                            headers: { 'Authorization' => customer1_auth_token }
     assert_response :success
   end
 
   test 'should get issues with Customer in the title' do
-    get :index, params: { title: Issue.first.title }
+    get api_v1_issues_path, params: { title: Issue.first.title }, 
+                            headers: { 'Authorization' => @customer1.auth_token }
     assert_response :success
     body = JSON.parse(response.body)
     assert_equal Issue.first.title, body['title']
